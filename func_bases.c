4ecc6e6f4f182cfe83938531f56eb0ccdbb19c38
Jareeyah <jariatdosunmu@gmail.com>
2023-04-25 04:53:29 -0700

    Adding comments and satisfying betty

--- func_bases.c
+++ func_bases.c
@@ -1,27 +1,40 @@
 #include "main.h"
 
+/**
+ * _print_octal - A function that prints a num in octal
+ * @num: int to be printed in octal
+ * @count: pointer to the int
+ * Return: The numbers of chars printed
+ */
 void _print_octal(unsigned int num, int *count)
 {
-    if (num >= 8)
-    {
-        _print_octal(num / 8, count);
-    }
-    _print_char((num % 8) + '0', count);
+	if (num >= 8)
+	{
+		_print_octal(num / 8, count);
+	}
+	_print_char((num % 8) + '0', count);
 }
 
+/**
+ * _print_hexadecimal - A function that prints a num in hexadecimal
+ * @num: num to be printed in hexadecimal
+ * @uppercase: int to print the hexadecimal in uppercase
+ * @count: pointer to the int
+ * Return: The number of characters to be printed
+ */
 void _print_hexadecimal(unsigned int num, int uppercase, int *count)
 {
-    char hex_digits[] = "0123456789abcdef";
-    char hex_digit;
+	char hex_digits[] = "0123456789abcdef";
+	char hex_digit;
 
-    if (num >= 16)
-    {
-        _print_hexadecimal(num / 16, uppercase, count);
-    }
-    hex_digit = hex_digits[num % 16];
-    if (uppercase)
-    {
-        hex_digit = toupper(hex_digit);
-    }
-    _print_char(hex_digit, count);
+	if (num >= 16)
+	{
+		_print_hexadecimal(num / 16, uppercase, count);
+	}
+	hex_digit = hex_digits[num % 16];
+	if (uppercase)
+	{
+		hex_digit = toupper(hex_digit);
+	}
+	_print_char(hex_digit, count);
 }
--- func_numbers.c
+++ func_numbers.c
@@ -1,21 +1,33 @@
 #include "main.h"
+
+/**
+ * _print_unsigned_decimal - function that prints an unsigned decimal
+ * @num: int to be printed in unsigned decimal
+ * @count: The pointer to the int
+ * Return: The number of chars printed
+ */
 void _print_unsigned_decimal(unsigned int num, int *count)
 {
-    if (num >= 10)
-    {
-        _print_unsigned_decimal(num / 10, count);
-    }
-    _print_char((num % 10) + '0', count);
+	if (num >= 10)
+	{
+		_print_unsigned_decimal(num / 10, count);
+	}
+	_print_char((num % 10) + '0', count);
 }
 
+/**
+ * _print_number - function that prints a number
+ * @num: The nummber to be printed
+ * @count: The pointer to the int
+ * Return: The number of chars printed
+ */
 void _print_number(int num, int *count)
 {
-    if (num < 0)
-    {
-        _print_char('-', count);
-        num = -num;
-    }
-
-    _print_unsigned_decimal(num, count);
+	if (num < 0)
+	{
+		_print_char('-', count);
+		num = -num;
+	}
+	_print_unsigned_decimal(num, count);
 }
 

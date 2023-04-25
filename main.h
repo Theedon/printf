a459bfd196c5cb2b0277c182be853c3d3deace4c
Jareeyah <jariatdosunmu@gmail.com>
2023-04-25 02:56:24 -0700

    editing the main.h

--- main.h
+++ main.h
@@ -5,7 +5,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 int _printf(const char *format, ...);
+void _print_char(char c, int *count);
+void _print_string(char *s, int *count);
+void _print_binary(unsigned int num, int *count);
+void _print_unsigned_decimal(unsigned int num, int *count);
+void _print_octal(unsigned int num, int *count);
+void _print_hexadecimal(unsigned int num, int uppercase, int *count);
 
 #endif /* MAIN_H */
